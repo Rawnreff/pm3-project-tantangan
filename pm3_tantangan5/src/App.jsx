f1f8d7f90b5c5ab4e8212307388fd86bb5fb15a4
Zhaidar <suholee75@gmail.com>
2025-08-27 10:21:21 +0700

    first commit

---
+++ pm3_tantangan5/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import CardProduk from "./components/CardProduk";
+import Counter from "./components/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Daftar Produk</h1>
+      <div className="produk-list">
+        <CardProduk 
+          nama="Bugetto Nuggets" 
+          harga="20000" 
+          deskripsi="Nugget sehat berbahan dasar jagung dan sayuran."
+        />
+        <CardProduk 
+          nama="Cookoories Premium" 
+          harga="15000" 
+          deskripsi="Kue kering sehat dengan bahan alami." 
+        />
+        <CardProduk 
+          nama="Nutricomm IoT" 
+          harga="500000" 
+          deskripsi="Perangkat IoT untuk monitoring kebun gizi." 
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
